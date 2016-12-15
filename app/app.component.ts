8e571ee3be4cdb063f8e44a47766a4076c6ab18b
Khris <kpaca@Mac-Air-5.local>
2016-12-14 21:41:17 -0600

    separate template file for app component

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,19 +3,12 @@ import { Component } from '@angular/core';
 @Component({
     moduleId: module.id,
     selector: 'app',
-    template: `
-    <app-title [title]="title" [subtitle]="subtitle"></app-title>
-    <nav>
-        <a routerLink="" routerLinkActive="active">Contact</a>
-        <a routerLink="/employee" routerLinkActive="active">Employees</a>
-    </nav>
-    <router-outlet></router-outlet>
-    `,
+    templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
     providers: []
 })
 
 export class AppComponent {
-    title = 'Hyphen Solutions Employee Portal';
+    title = 'HS Employee Portal';
     subtitle = '';
 }
